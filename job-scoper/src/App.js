9b1352de89f9287ed84f506ba16b42f8684e106b
dstan <64676122+dwstan@users.noreply.github.com>
2024-04-28 11:25:08 -0700

    added maps

--- job-scoper/src/App.js
+++ job-scoper/src/App.js
@@ -4,6 +4,8 @@ import WalkAbility from './Views/WalkAbility';
 import WomansRights from './Views/WomansRights';
 import LGBTRights from './Views/LGBTRights';
 import ResumeHelper from './Views/ResumeHelper';
+import MapContainer from './Views/MapContainer';
+
 
 function App() {
   // State to hold the input value
@@ -25,6 +27,8 @@ function App() {
   const [genderafirm_legality, setGenderAfirmLegality] = useState(0);
   const [employment_discrimination, setEmploymentDiscrimination] = useState(0);
   const [abortion_policy, setAbortionPolicy] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
 
   // Function to handle input changes
   const handleInputChange = (event) => {
@@ -60,6 +64,10 @@ function App() {
       setGenderAfirmLegality(data.genderafirm_legality);
       setEmploymentDiscrimination(data.employment_discrimination);
       setAbortionPolicy(data.abortion_policy);
+      setLat(data.lat);
+      setLon(data.lon);
+      console.log(lat)
+      console.log(lon)
       setDataFetched(true);  // Set data fetched to true
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
@@ -92,11 +100,17 @@ function App() {
             <LGBTRights ei={ei} ei_legal={ei_legal} ei_po={ei_po} genderafirm_legality={genderafirm_legality} employment_discrimination={employment_discrimination}/>
           </div>
 
+          <div style ={{marginTop: '30px', marginBottom: "500px"}}>
+          <MapContainer latIn={lat} lonIn={lon} />
+          </div>
+
+          <div>
           <ResumeHelper jobLink = {inputValue}/>
+          </div>
         </div>
       )}
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ job-scoper/src/Views/MapContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+
+const MapContainer = (props) => {
+    const mapStyles = {
+        width: '50%',
+        height: '500px',
+        margin: '0 auto',
+        borderRadius: '10px'
+    };
+  
+    const { latIn, lonIn } = props;
+  
+    return (
+        <div>
+        <Map
+            google={props.google}
+            zoom={10}
+            style={mapStyles}
+            initialCenter={{
+            lat: parseInt(latIn),
+            lng: parseInt(lonIn)
+            }}
+        >
+            <Marker position={{ lat: parseInt(latIn), lng: parseInt(lonIn) }} />
+        </Map>
+        </div>
+    );
+  };
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAfiq1A9iQ8unLOPZA8RodYSFiLBFQyflA"
+
+})(MapContainer);
